dd9adb6865b882a03cdb4ad39e26cd45c3d2dcfa
MIREKZXD <miroslav@chada.cz>
2024-04-24 23:25:08 +0200

    Listening to KeyDown Action in GameView

--- Scrabblos/MVVM/View/GameView.xaml.cs
+++ Scrabblos/MVVM/View/GameView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Scrabblos.MVVM.View;
@@ -9,11 +10,19 @@ namespace Scrabblos.MVVM.View;
 public partial class GameView : UserControl {
     public GameView() {
         InitializeComponent();
+    }
 
+    private void GameView_OnLoaded(object sender, RoutedEventArgs e) {
         MainWindow.Instance.OnAnyKeyDown += OnAnyKeyDown;
     }
 
-    private void OnAnyKeyDown(Key key) {
-        throw new NotImplementedException();
+    private void GameView_OnUnloaded(object sender, RoutedEventArgs e) {
+        MainWindow.Instance.OnAnyKeyDown -= OnAnyKeyDown;
+    }
+
+    private void OnAnyKeyDown(Key key)
+    {
+        MessageBox.Show(key.ToString());
+        TbPressed.Text = key.ToString();
     }
 }
--- Scrabblos/MVVM/ViewModel/GameViewModel.cs
+++ Scrabblos/MVVM/ViewModel/GameViewModel.cs
@@ -1,4 +1,27 @@
-﻿namespace Scrabblos.MVVM.ViewModel;
+﻿using Scrabblos.Core;
+using Scrabblos.Services;
 
-internal class GameViewModel : Core.ViewModel {
+namespace Scrabblos.MVVM.ViewModel;
+
+internal class GameViewModel : Core.ViewModel
+{
+    private INavigationService _navigation;
+
+    public INavigationService Navigation
+    {
+        get => _navigation;
+        set
+        {
+            _navigation = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public RelayCommand NavigateToHomeCommand { get; set; } // Příkaz pro přepnutí View
+
+    public GameViewModel(INavigationService navService)
+    {
+        Navigation = navService;
+        NavigateToHomeCommand = new RelayCommand(o => { Navigation.NavigateTo<HomeViewModel>(); }, o => true); // Definování příkazu
+    }
 }
