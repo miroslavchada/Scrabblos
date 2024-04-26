fba41d08d5eaf1c64a7a3c5ac339ceb8932d9680
Miroslav Chada <miroslav@chada.cz>
2024-04-26 15:23:13 +0200

    Dragable tile

--- Scrabblos/MVVM/View/GameView.xaml.cs
+++ Scrabblos/MVVM/View/GameView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace Scrabblos.MVVM.View;
 
@@ -13,6 +15,36 @@ public partial class GameView : UserControl {
     }
 
     private bool escapeMenu = false;
+
+    protected bool isDragging;
+    private Point clickPosition;
+    private TranslateTransform originTT;
+
+    private void Tile_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        var draggableControl = sender as Image;
+        originTT = draggableControl.RenderTransform as TranslateTransform ?? new TranslateTransform();
+        isDragging = true;
+        clickPosition = e.GetPosition(this);
+        draggableControl.CaptureMouse();
+    }
+
+    private void Tile_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+        isDragging = false;
+        var draggable = sender as Image;
+        draggable.ReleaseMouseCapture();
+    }
+
+    private void Tile_MouseMove(object sender, MouseEventArgs e) {
+        var draggableControl = sender as Image;
+        if (isDragging && draggableControl != null) {
+            Point currentPosition = e.GetPosition(this);
+            var transform = draggableControl.RenderTransform as TranslateTransform ?? new TranslateTransform();
+            transform.X = originTT.X + (currentPosition.X - clickPosition.X) * GetActualWidthMultiplier();
+            transform.Y = originTT.Y + (currentPosition.Y - clickPosition.Y) * GetActualHeightMultiplier();
+            draggableControl.RenderTransform = new TranslateTransform(transform.X, transform.Y);
+        }
+    }
 
     private void GameView_OnLoaded(object sender, RoutedEventArgs e) {
         MainWindow.Instance.OnAnyKeyDown += OnAnyKeyDown;
@@ -34,10 +66,15 @@ public partial class GameView : UserControl {
                 }
                 escapeMenu = !escapeMenu;
                 break;
-            default:
-                TbPressed.Text = $"Zmáčknuté tlačítko: {key}";
-                MessageBox.Show(key.ToString());
-                break;
         }
+    }
+
+    private double GetActualHeightMultiplier()
+    {
+        return 2160 / GameViewBox.ActualHeight;
+    }
+
+    private double GetActualWidthMultiplier() {
+        return 2880 / GameViewBox.ActualWidth;
     }
 }
